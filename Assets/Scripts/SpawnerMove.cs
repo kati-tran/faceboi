b86b0bafc5a36f62494fb335cdd7831df094a8ca
Kati Tran <katit1@uci.edu>
2020-02-02 01:19:35 -0800

    lanes i guess

--- Assets/Scripts/SpawnerMove.cs
+++ Assets/Scripts/SpawnerMove.cs
@@ -4,10 +4,11 @@ public class SpawnerMove : MonoBehaviour
 {
 	public Transform player;	// A variable that stores a reference to our Player
 	//public Vector3 offset;		// A variable that allows us to offset the position (x, y, z)
+	public float spawnerDistance = 100f;
 	
 	// Update is called once per frame
 	void Update () {
 		// Set our position to the players position and offset it
-		transform.position = new Vector3(0, 1, player.position.z + 100);
+		transform.position = new Vector3(0, 1, player.position.z + spawnerDistance);
 	}
 }
