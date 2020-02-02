43ee6d0457c83e0d3d4b538a667ff0467f5c1f5b
alexkumar520 <43835304+alexkumar520@users.noreply.github.com>
2020-02-01 19:17:27 -0800

    gun

---
+++ Assets/Gun.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 1000f;
+    public float range = 100f; //how far it can shoot in one direction. 
+    // Start is called be
+    public GameObject g;
+     void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))//"shooting"
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(g.transform.position,g.transform.forward, out hit, range))//condition for if in front of block
+        {
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.takeDamage(damage);
+            }//add visual effect for destroy here. or actually in target. 
+        }
+    }
+}
