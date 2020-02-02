45e2ae7bec35b92c4d11a7c65c022897d3d0f748
Kati Tran <katit1@uci.edu>
2020-02-01 22:00:35 -0800

    Added face obstacles

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,6 +5,7 @@ public class Spawner : MonoBehaviour
 {
     
     public GameObject[] enemies;
+    public GameObject[] facers;
     //public Vector3 spawnValues;
     public float spawnWait;
     public float spawnMostWait;
@@ -46,16 +47,32 @@ public class Spawner : MonoBehaviour
     			int count = Random.Range(1,5);
     			while(count != 0){
 	    			int test = GetRandomPosition();
-	    			Vector3 spawnPosition = new Vector3 (test, 1, Random.Range(-10,20));
+	    			Vector3 spawnPosition = new Vector3 (test, 4, Random.Range(-10,20));
 	    			GameObject clone = Instantiate (enemies[randEnemy], spawnPosition + transform.TransformPoint (0,0,0), gameObject.transform.rotation);
+	    			//FACERRRR
 	    			count--;
     			}
+    			Vector3 facspot = new Vector3(GetRandomPosition(), 4, 1);
+	    		GameObject face = Instantiate (facers[0], facspot + transform.TransformPoint (0,0,0), gameObject.transform.rotation);
     		}
 
     		if(randEnemy == 1){
     			int negThreeorThree = Random.Range(0, 2) == 0 ? -3 : 3;
-    			Vector3 spawnPosition = new Vector3(negThreeorThree, 1, 1);
-				GameObject clone = Instantiate (enemies[randEnemy], spawnPosition + transform.TransformPoint (0,0,0), gameObject.transform.rotation);    			
+    			Vector3 spawnPosition = new Vector3(negThreeorThree, 4, 1);
+				GameObject clone = Instantiate (enemies[randEnemy], spawnPosition + transform.TransformPoint (0,0,0), gameObject.transform.rotation); 
+
+				//facerrrr 
+				int facc;
+
+				if(negThreeorThree == -3){
+					facc = 12;
+				}
+				else{
+					facc = -12;
+				}
+
+				Vector3 facspot = new Vector3(facc, 4, 1);
+				GameObject face = Instantiate (facers[0], facspot + transform.TransformPoint (0,0,0), gameObject.transform.rotation);  			
     			Destroy (clone, 5.0f);
     		}
 
@@ -66,10 +83,14 @@ public class Spawner : MonoBehaviour
     		}
 
     		if(randEnemy == 3){
-    			Vector3 spawnPosition = new Vector3(-9, 0, 0);
-    			Vector3 spawnPosition2 = new Vector3(9, 0, 0);
+    			Vector3 spawnPosition = new Vector3(-9, 4, 0);
+    			Vector3 spawnPosition2 = new Vector3(9, 4, 0);
     			GameObject clone = Instantiate (enemies[randEnemy], spawnPosition + transform.TransformPoint (0,0,0), gameObject.transform.rotation);
     			GameObject clone2 = Instantiate (enemies[randEnemy], spawnPosition2 + transform.TransformPoint (0,0,0), gameObject.transform.rotation);
+
+				Vector3 facspot = new Vector3(0, 4, 0);
+				GameObject face = Instantiate (facers[0], facspot + transform.TransformPoint (0,0,0), gameObject.transform.rotation);
+
     			Destroy (clone, 5.0f);
     			Destroy (clone2, 5.0f);
     		}
