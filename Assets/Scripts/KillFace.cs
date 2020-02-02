a0426bdebb5e5f0d332d1248962715b4e502c565
alexkumar520 <43835304+alexkumar520@users.noreply.github.com>
2020-02-01 22:36:28 -0800

    facer not working

---
+++ Assets/Scripts/KillFace.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+public class KillFace : MonoBehaviour
+{
+    public float health = 1f;
+
+    public void takeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
